6920c60a1f1988aca5cb97b179c7bc6f69f36056
Ryoto Saito <ryoto.no.address@gmail.com>
2019-08-13 11:22:41 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='keras-centernet',
+    version='0.0.0',
+    description='Keras implementation for CenterNet',
+    url='https://github.com/seqsense/keras-centernet',
+    author='SEQSENSE',
+    packages=['keras-centernet'],
+    install_requires=[
+        'Keras>2.2.4'
+        'opencv-python>3.4.3.18'
+        'tqdm>4.26.0'
+        #'youtube-dl>2019.4.30'
+        'pytest>4.4.1'
+        'Pillow>6.0.0'
+        'matplotlib>3.0.3'
+        'Cython>0.29.7'
+        'pycocotools>2.0.0'
+    ],
+    zip_safe=True,
+    license='Other/Proprietary License',
+)
